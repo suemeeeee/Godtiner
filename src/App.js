35eac783d4b5dfcfa0c24c3194a2340054c27d8b
suemeeeee <yungmi2909@gmail.com>
2022-10-30 01:50:50 +0900

    GABOJAGO

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>소영아 내가 보이니? ... </h2>
-      <h3>영미야 내가 보이니?...</h3>
-      <h4>이게 ... 대체... 뭐람</h4>
-      <h5>지금 케로로 틀고 git 조지는 중</h5>
+      <h2>졸프... 부셔주겠어.</h2>
     </div>
   );
 }
