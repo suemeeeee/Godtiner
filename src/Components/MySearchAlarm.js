b5170a8dca7b778557e711f0c084d439c9e04b09
Narumi <sychoi2008@naver.com>
2022-10-30 20:34:55 +0900

    App.css와 MySearchAlarm.js에서 검색창 제작

--- src/Components/MySearchAlarm.js
+++ src/Components/MySearchAlarm.js
@@ -1,3 +1,16 @@
-const MySearchAlarm = () => {};
+import { useState } from "react";
+
+const MySearchAlarm = () => {
+  const [search, setSearch] = useState("");
+  return (
+    <div className="MySearchAlarm">
+      <input
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+};
 
 export default MySearchAlarm;
