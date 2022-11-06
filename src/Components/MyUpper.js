69e5938924ceb05ce721ce057814bfabbc868315
suemeeeee <yungmi2909@gmail.com>
2022-11-06 17:16:18 +0900

    MyUpper 화살표 수정, Login창 버튼 이펙트 수정

--- src/Components/MyUpper.js
+++ src/Components/MyUpper.js
@@ -4,8 +4,16 @@ const MyUpper = ({ text }) => {
   const navigate = useNavigate();
   return (
     <div className="MyUpper">
-      <button className="backButton" onClick={() => navigate(-1)}>
-        {"←"}
+      <button
+        className="backButton"
+        onClick={() => navigate(-1)}
+        style={{ padding: "20px" }}
+      >
+        <img
+          src="img/arrow.png"
+          alt="arrow"
+          style={{ width: "30px", height: "30px" }}
+        />
       </button>
       <span className="pageDescript">{text}</span>
     </div>
