20da8018da8c1402c29e84bc60af0a5f6c42bb67
suemeeeee <yungmi2909@gmail.com>
2022-11-12 00:27:24 +0900

    week Tab 클릭 시 그 날짜를  중앙으로 이동

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,14 +1,26 @@
-import React, { useEffect, useState, Dispatch, SetStateAction } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import { ko } from "date-fns/esm/locale";
 import format from "date-fns/format";
+import Form from "react-bootstrap/Form";
 import "react-datepicker/dist/react-datepicker.css"; // 이렇게 가져와야 기본 디자인이 설정된다
 
 import "./Calendar.css";
 
 const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const [visible, setVisible] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (e) => {
+    setIsOpen(!isOpen);
+    setStartDate(e);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   function addDays(date, days) {
@@ -21,40 +33,41 @@ const Calendar = () => {
     <div>
       <div className="calendarHeader">
         {format(startDate, "yyyy년 MM월 ")}
-        <button
-          onClick={() => {
-            setVisible(!visible); //버튼 클릭 시 달력 visible
-          }}
-        >
-          달력
-        </button>
+        <button onClick={handleClick}>달력</button>
       </div>
-      {visible && (
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => {
-            setStartDate(date);
-            setVisible(!visible); //모달 팝업창으로 바꿔야 할 듯.. 날짜 선택 시 달력 안 보이게
-          }}
-          inline
-        />
+      {isOpen && (
+        <DatePicker selected={startDate} onChange={handleChange} inline />
       )}
 
       <div className="week_btn">
-        {" "}
-        <button>
-          {week[startDate.getDay() - 3]}
+        <button
+          className="week-3"
+          onClick={() => {
+            setStartDate(addDays(startDate, -3));
+          }}
+        >
+          {week[addDays(startDate, -3).getDay()]}
           <br />
           {format(addDays(startDate, -3), "dd")}
           <br />
         </button>
-        <button>
-          {week[startDate.getDay() - 2]}
+        <button
+          className="week-2"
+          onClick={() => {
+            setStartDate(addDays(startDate, -2));
+          }}
+        >
+          {week[addDays(startDate, -2).getDay()]}
           <br />
           {format(addDays(startDate, -2), "dd")}
         </button>
-        <button>
-          {week[startDate.getDay() - 1]}
+        <button
+          className="week-1"
+          onClick={() => {
+            setStartDate(addDays(startDate, -1));
+          }}
+        >
+          {week[addDays(startDate, -1).getDay()]}
           <br />
           {format(addDays(startDate, -1), "dd")}
         </button>
@@ -64,17 +77,32 @@ const Calendar = () => {
           {format(startDate, "dd")}
         </button>
         {/* 선택 날짜 */}
-        <button>
+        <button
+          className="week1"
+          onClick={() => {
+            setStartDate(addDays(startDate, 1));
+          }}
+        >
           {week[addDays(startDate, 1).getDay()]}
           <br />
           {format(addDays(startDate, 1), "dd")}
         </button>
-        <button>
+        <button
+          className="week2"
+          onClick={() => {
+            setStartDate(addDays(startDate, -2));
+          }}
+        >
           {week[addDays(startDate, 2).getDay()]}
           <br />
           {format(addDays(startDate, 2), "dd")}
         </button>
-        <button>
+        <button
+          className="week3"
+          onClick={() => {
+            setStartDate(addDays(startDate, 3));
+          }}
+        >
           {week[addDays(startDate, 3).getDay()]}
           <br />
           {format(addDays(startDate, 3), "dd")}
