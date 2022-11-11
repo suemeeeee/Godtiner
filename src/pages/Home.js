811e3a7fdeecbc6624eaeb3690f63b6099c727b5
suemeeeee <yungmi2909@gmail.com>
2022-11-12 00:37:27 +0900

    Home.js에서 calendar.js 로 빼낸 부분 지움, Calendar.css 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,43 +11,14 @@ import ShareIcon from "@mui/icons-material/Share";
 
 const Home = () => {
   const routineList = useContext(RoutineStateContext);
-  //newDate객체는 바로 날짜로 변환해주지 않는다.
-  // 따라서, toISOString메서드를 사용한다. 이 메서드는 Date객체를 받아 yyyy-mm-dd-블라블라 형태로 반환
-  // 이에 인덱싱을 통하여
-  const getStringDate = (date) => {
-    return date.toISOString().slice(0, 10);
-  };
-
   const navigate = useNavigate();
-
-  //날짜를 저장하는 state
-  const [curDate, setCurDate] = useState(new Date());
 
   const [data, setData] = useState(routineList);
 
-  //왜 +만 텍스트로 더해지지?
   return (
     <div className="Home">
       <MySearchAlarm />
-      {/* Calendar컴포넌트 안에 버튼 눌렀을 때 캘린더 뜰 수 있게 하는걸로 수정하는 편이 낫다고 생각 */}
-      {/* <div className="current_date">
-        <input
-          className="input_date"
-          type="date"
-          value={curDate}
-          onChange={(e) => setCurDate(e.target.value)}
-        />
-      </div> */}
       <Calendar />
-      {/* <div className="date_btn">
-        <button>{curDate.slice(-2) - 3}</button>
-        <button>{curDate.slice(-2) - 2}</button>
-        <button>{curDate.slice(-2) - 1}</button>
-        <button>{curDate.slice(-2) - 0}</button>
-        <button>{curDate.slice(-2) + 1}</button>
-        <button>{curDate.slice(-2) + 2}</button>
-        <button>{curDate.slice(-2) + 3}</button>
-      </div> */}
       <ShareIcon onClick={() => navigate("/shareroutine")} />
       <div className="routinSection">
         {data.map((it) => (
