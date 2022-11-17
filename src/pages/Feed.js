8c410927a4e0419e8bd83ccb84cd43e37ef6920f
suemeeeee <yungmi2909@gmail.com>
2022-11-18 00:39:54 +0900

    주석 추가

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -12,6 +12,7 @@ const Feed = () => {
       <h2>🔍최신 루틴</h2>
       <div className="Routine_list">
         <div>
+          {/* map() 함수 써서 더미데이터를 반복적으로 띄워줘야 할 듯.. 근데 아직 map 함수 약해서 공부해 올게 ㅜㅜ */}
           <img></img>
           <text></text>
           <div></div>
