38d8abc96cf454183888eebb1bf6984bf5633649
suemeeeee <yungmi2909@gmail.com>
2022-11-25 02:47:14 +0900

    tag 검색 기능 추가 ..! 너무 짜릿해 ㅜㅜ

--- src/Components/MySearchAlarm.js
+++ src/Components/MySearchAlarm.js
@@ -6,19 +6,26 @@ import SearchIcon from "@mui/icons-material/Search";
 import Notifications from "@mui/icons-material/Notifications";
 
 import feedDummyData from "../DummyData/feedDummyData.json";
-import { PropaneSharp } from "@mui/icons-material";
 
 const MySearchAlarm = ({ parentFunction }) => {
   const navigate = useNavigate();
   //검색창의 검색어 변경을 다루기 위한 hooks
   const [search, setSearch] = useState("");
 
-  //일단 태그 말고 타이틀로 검색만 구현 함
-  //조건 일치하여 검색된 결과담는 변수
+  //조건 일치하여 검색된 결과담는 변수 = filterData
   //검색창에 검색어 없으면 아무것도 안뜨게 수정해야 함
-  const filterData = feedDummyData.Feed_Routine.filter((p) => {
-    return p.RoutineTitle.includes(search);
-  });
+  // -> 굳이? 걍 다 뜨게 하지 뭐 ㅋ
+  let filterData = undefined;
+
+  if (search.charAt(0) === "#") {
+    filterData = feedDummyData.Feed_Routine.filter((p) => {
+      return p.RoutineTag.includes(search.substring(1));
+    });
+  } else {
+    filterData = feedDummyData.Feed_Routine.filter((p) => {
+      return p.RoutineTitle.includes(search);
+    });
+  }
 
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
@@ -32,6 +39,7 @@ const MySearchAlarm = ({ parentFunction }) => {
   //아래는 검색버튼 눌렀을 때 함수
   const onClickSearch = (e) => {
     parentFunction(filterData);
+    console.log(search.charAt(0));
     //props로 SearchPage에 검색 결과 배열 넘겨 줌
   };
 
