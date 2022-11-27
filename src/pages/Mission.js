80fdc20e215819859cb313d7bedc5f7b4e0b37e2
suemeeeee <yungmi2909@gmail.com>
2022-11-28 01:43:04 +0900

    미션 탭 껍데기만 짜는데 머리 터질 거 같아서 그만할래 ...

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,10 +1,54 @@
 import { useState } from "react";
+import "./Mission.css";
 import MoveTab from "../Components/MoveTab";
+import { color } from "@mui/system";
+import { Instagram } from "@material-ui/icons";
 
 const Mission = () => {
+  const [trophySrc, setTrophySrc] = useState();
+
+  function toggleImg() {
+    var img_src;
+    img_src = "./image/ColorTrophy.png";
+    return img_src;
+  }
+
   return (
     <div>
       <h1>Mission🏆</h1>
+      <div className="mission_div">
+        <div className="trophy_div">
+          <ul className="ul_ms">
+            <li className="li_ms">
+              <img
+                className="trophy_img trophy_50"
+                src="./img/Trophy.png"
+              ></img>
+              <h3 className="trophy_text">미션 50개 달성</h3>
+            </li>
+            <li className="li_ms">
+              <img className="trophy_img" src="./img/Trophy.png"></img>
+              <h3 className="trophy_text">미션 100개 달성</h3>
+            </li>
+            <li className="li_ms">
+              <img className="trophy_img" src="./img/Trophy.png"></img>
+              <h3 className="trophy_text">미션 200개 달성</h3>
+            </li>
+            <li className="li_ms">
+              <img className="trophy_img" src="./img/Trophy.png"></img>
+              <h3 className="trophy_text">미션 500개 달성</h3>
+            </li>
+            <li className="li_ms">
+              <img className="trophy_img" src="./img/Trophy.png"></img>
+              <h3 className="trophy_text">미션 800개 달성</h3>
+            </li>
+            <li className="li_ms">
+              <img className="trophy_img" src="./img/Trophy.png"></img>
+              <h3 className="trophy_text">미션 1000개 달성</h3>
+            </li>
+          </ul>
+        </div>
+      </div>
       <MoveTab />
     </div>
   );
