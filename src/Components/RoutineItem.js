b6ad11db6110ac12ae712b06ae0456d5c6da1a5e
suemeeeee <yungmi2909@gmail.com>
2022-11-30 02:32:15 +0900

    루틴 체크박스 선택 시 clear 변수 true 로 바꾸고 css 변경 (근데 오류.. )

--- src/Components/RoutineItem.js
+++ src/Components/RoutineItem.js
@@ -1,21 +1,41 @@
 import "./RoutineItem.css";
 import { useNavigate } from "react-router-dom";
+import MyRoutineDummyData from "../DummyData/MyRoutineDummyData.json";
+import { isClickableInput } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
 
-const RoutineItem = ({ id, content, startTime, endTime }) => {
+const RoutineItem = ({ id, content, startTime, endTime, isclear }) => {
   // home화면에서 세부 루틴들을 보여줌
 
   const navigate = useNavigate();
+
+  //체크박스 누르면 루틴 클리어 되면서 지워지는 함수
+  const routineClear = (e) => {
+    isclear = !isclear;
+    console.log(isclear);
+    if (isclear === true) {
+      document.getElementById("gg").className = "clearedRoutine";
+    } else {
+      document.getElementById("gg").className = "MyRoutineContent";
+    }
+  };
+
+  const setClassNameClear = () => {
+    navigate(`/myroutineeditor/${id}`);
+  };
   return (
     <div className="routineGroup">
       <div className="time">
         {startTime}-{endTime}
       </div>
       <button
-        className="RoutineContent"
-        onClick={() => navigate(`/myroutineeditor/${id}`)}
+        id="gg"
+        className={"MyRoutineContent"}
+        onClick={setClassNameClear}
       >
         {content}
       </button>
+      <input value={id} type="checkbox" onClick={routineClear} />
     </div>
   );
 };
